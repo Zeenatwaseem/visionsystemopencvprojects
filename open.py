612135f03387f81808d9d9076970430681c24921
Zeenatwaseem <101622552+Zeenatwaseem@users.noreply.github.com>
2022-09-12 22:49:17 +0530

    Add files via upload

---
+++ open.py
@@ -0,0 +1,10 @@
+import cv2
+#image capture project
+print("zeenat waseem")
+#open image and read
+img = cv2.imread("OIP.jpg", cv2.IMREAD_COLOR)
+cv2.imshow("output",img)
+cv2.waitKey(0)#0 means infinite delay
+
+
+
